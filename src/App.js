dc3cb058165fc9338f9ffcd65b9599f1817418b5
lyapkin <lyapkin992@gmail.com>
2023-05-03 21:33:06 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Field from './components/Field';
+import Result from './components/Result';
+import StartButton from './components/StartButton';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [testState, setTestState] = useState({
+		running: false,
+		done: false,
+		result: null
+	}) 
+	
+	const handleStart = () => {
+		setTestState({
+			running: true,
+			done: false,
+			result: null
+		})
+	}
+
+	const handleReset = () => {
+		setTestState({
+			running: false,
+			done: false,
+			result: null
+		})
+	}
+
+
+	return (
+		<div className="App">
+			{
+				(
+					testState.running && <Field setTestState={setTestState} >
+									  		<StartButton handleClick={handleReset} text={'Начать заново'} />
+									  	 </Field>
+				) ||
+
+				(
+					testState.done && <Result speed={testState.result.speed} accuracy={testState.result.accuracy} >
+									  	  <StartButton handleClick={handleReset} text={'Начать заново'} />
+									  </Result>
+				) ||
+
+				<StartButton handleClick={handleStart} text={'Начать печатать'} />
+			}
+		</div>
+	);
 }
 
 export default App;
